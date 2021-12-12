9d3529ea1f790be42100a0129e0caecb6502e144
Farzan-Far <farzankhalaj76@gmail.com>
2021-12-12 20:34:47 +0330

    RegisterForm

---
+++ src/main/java/com/example/registerformlogin/RegisterFormLoginApplication.java
@@ -0,0 +1,15 @@
+package com.example.registerformlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterFormLoginApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RegisterFormLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registerformlogin/RegisterFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerformlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterFormLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
